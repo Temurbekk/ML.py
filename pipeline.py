323c71c4eefad8fc57615df7bf71435da8a2107f
Temurbek Sabirov <prodota150@gmail.com>
2020-12-06 13:41:11 -0500

    Implement custom KNN classifier

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,39 @@
-from sklearn import datasets, tree
+from sklearn import datasets
+# from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+from scipy.spatial import distance
+
+
+def euc(a, b):
+    return distance.euclidean(a, b)
+
+
+class ScrappyKNN():
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def predict(self, X_test):
+        predictions = []
+        for row in X_test:
+            label = self.closest(row)
+            predictions.append(label)
+
+        return predictions
+
+    def closest(self, row):
+        best_dist = euc(row, self.X_train[0])
+        best_index = 0
+
+        for i in range(1, len(self.X_train)):
+            dist = euc(row, self.X_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+        return self.y_train[best_index]
+
 
 iris = datasets.load_iris()
 
@@ -10,7 +43,7 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5)
 
-my_classifier = tree.DecisionTreeClassifier()
+my_classifier = ScrappyKNN()
 
 my_classifier.fit(X_train, y_train)
 
